b8b96766ee68d001a0ad5ba3fd3ca1c4c8ca985b
Akiko Takano <akiko.pusu@gmail.com>
2012-01-13 04:35:51 +0900

    view template側でレンダリングするように修正しました。(IssueID: #908)     (Ref. http://projects.andriylesyuk.com/issues/1827)

--- lib/banner_application_hooks.rb
+++ lib/banner_application_hooks.rb
@@ -1,48 +1,13 @@
 class BannerApplicationHooks < Redmine::Hook::ViewListener
   def view_layouts_base_html_head(context = {})
     return '' unless Setting.plugin_redmine_banner['enable'] == "true"
-    o = ''
-    o << stylesheet_link_tag('banner', :plugin => 'redmine_banner')
-#    o << '<script type="text/javascript">'
-#    o << 'Event.observe(window, \'load\', window_onload);'
-#    o << '  function window_onload(evt){'
-#    o << "    $( \'top-menu\' ).insert({after:\'"
-#    o << textilize_banner(Setting.plugin_redmine_banner['banner_description'])
-#    o << '\'});'
-#    o << '}'
-#    o << '</script>'
-
-   return o
-  end
-
-  def view_layouts_base_body_bottom(context = {})
-    return '' unless Setting.plugin_redmine_banner['enable'] == "true"
-    o = ''
-    o << textilize_banner(Setting.plugin_redmine_banner['banner_description'])
-    return o 
-  end
-
-  def view_layouts_base_after_top_menu(context = {})
-    return '' unless Setting.plugin_redmine_banner['enable'] == "true"
-    o = ''
-    o << textilize_banner(Setting.plugin_redmine_banner['banner_description'])
+    o = stylesheet_link_tag('banner', :plugin => 'redmine_banner')
     return o
   end
 
-  private
-  def escape_single_quote(data)
-    return data.gsub("'","&rsquo;")
-  end
-
-  def textilize_banner(data)
-    o = ''
-    o << '<div class="banner banner_'
-    o << Setting.plugin_redmine_banner['type']
-    o << '">'
-    o << escape_single_quote(textilizable(data))
-    o << '</div>'
-    return o 
-  end
-
+  # TODO: view_layouts_base_after_top_menu is not supported Redmine itself. (Submitted ticket: http://www.redmine.org/issues/9915)
+  render_on :view_layouts_base_body_bottom, :partial => 'banner/body_bottom'
+  render_on :view_layouts_base_after_top_menu, :partial => 'banner/after_top_menu'
+  
 end
 

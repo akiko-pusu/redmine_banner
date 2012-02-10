4f0c31b34d300ef72279d466bd3cf54c34174417
haru_iida <>
2012-02-10 22:52:54 +0900

    Use javascript instead of replacing base.html.erb.

--- lib/banner_application_hooks.rb
+++ lib/banner_application_hooks.rb
@@ -7,7 +7,8 @@ class BannerApplicationHooks < Redmine::Hook::ViewListener
 
   # TODO: view_layouts_base_after_top_menu is not supported Redmine itself. (Submitted ticket: http://www.redmine.org/issues/9915)
   render_on :view_layouts_base_body_bottom, :partial => 'banner/body_bottom'
-  render_on :view_layouts_base_after_top_menu, :partial => 'banner/after_top_menu'
-  
 end
 
+class BannerTopMenuHooks < Redmine::Hook::ViewListener
+  render_on :view_layouts_base_body_bottom, :partial => 'banner/after_top_menu'  
+end

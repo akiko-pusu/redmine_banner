8da3d2d0e223ee2c871ef5df7cbe67af4d84ee17
Akiko Takano <akiko.pusu@gmail.com>
2012-02-12 06:09:33 +0900

    Change version number to 0.0.3 and modify readme. (IssueID: #948)

--- lib/banner_application_hooks.rb
+++ lib/banner_application_hooks.rb
@@ -5,10 +5,11 @@ class BannerApplicationHooks < Redmine::Hook::ViewListener
     return o
   end
 
-  # TODO: view_layouts_base_after_top_menu is not supported Redmine itself. (Submitted ticket: http://www.redmine.org/issues/9915)
   render_on :view_layouts_base_body_bottom, :partial => 'banner/body_bottom'
 end
 
+# TODO: view_layouts_base_after_top_menu is not supported Redmine itself. 
+# Now use javascript to insert after top-menu. (Submitted ticket: http://www.redmine.org/issues/9915)  
 class BannerTopMenuHooks < Redmine::Hook::ViewListener
   render_on :view_layouts_base_body_bottom, :partial => 'banner/after_top_menu'  
 end

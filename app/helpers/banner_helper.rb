0a8571d1c9a2533c3ef8754620ae72213c38f7bf
Akiko Takano <akiko.pusu@gmail.com>
2012-02-15 01:38:19 +0900

    Remove unused code and add timer support code. (IssueID: #935)
    Signed-off-by: Akiko Takano <akiko.pusu@gmail.com>

--- app/helpers/banner_helper.rb
+++ app/helpers/banner_helper.rb
@@ -1,5 +1,6 @@
-module BannerHelper
-  def escape_single_quote(data)
-    return data.gsub("'","&rsquo;") 
-  end
+module BannerHelper  
+  def get_time(ymd, h, m)
+    d = Date.strptime(ymd, "%Y-%m-%d")
+    return Time.mktime(d.year, d.month, d.day, h.to_i, m.to_i)
+  end  
 end

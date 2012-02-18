3763359b77a45fff3921affefe2cd509b4557a35
Akiko Takano <akiko.pusu@gmail.com>
2012-02-19 00:02:49 +0900

    Add test code.
    Signed-off-by: Akiko Takano <akiko.pusu@gmail.com>

--- test/unit/banner_application_hooks_test.rb
+++ test/unit/banner_application_hooks_test.rb
@@ -11,7 +11,6 @@ class BannerApplicationHooksTest < Test::Unit::TestCase
     context = {}
     Setting.plugin_redmine_banner['use_timer'] = "false"
     assert_equal true, target.is_pass_timer?(context)
-
   end
   
   def test_is_pass_timer_true_for_passed_time_should_be_false
@@ -43,7 +42,7 @@ class BannerApplicationHooksTest < Test::Unit::TestCase
     Setting.plugin_redmine_banner['end_ymd'] = "1999-12-31"
     Setting.plugin_redmine_banner['end_hour'] = "01"
     Setting.plugin_redmine_banner['end_min'] = "01"       
-    assert_equal false, target.is_pass_timer?(context)   
+    assert(!target.is_pass_timer?(context))   
 
   end
 
@@ -62,6 +61,46 @@ class BannerApplicationHooksTest < Test::Unit::TestCase
     Setting.plugin_redmine_banner['end_min'] = "01"       
     assert_raise(ArgumentError){ target.is_pass_timer?(context) }   
 
-  end  
+  end
 
+  def  test_should_not_display_header?
+    target = BannerMessageHooks.instance
+    context = {}
+    Setting.plugin_redmine_banner['enable'] = "true"
+    Setting.plugin_redmine_banner['display_part'] = "footer"
+    Setting.plugin_redmine_banner['use_timer'] = "false"
+    assert(!target.should_display_header?(context))
+  end
+  
+  def test_should_display_header?
+    target = BannerMessageHooks.instance
+    context = {}
+    Setting.plugin_redmine_banner['enable'] = "true"
+    Setting.plugin_redmine_banner['display_part'] = "header"
+    Setting.plugin_redmine_banner['use_timer'] = "false"
+    assert(target.should_display_header?(context))
+    
+    # between test
+    now = Time.now
+    start_time = now - 60*60*24
+    end_time = now + 60*60*24
+    Setting.plugin_redmine_banner['use_timer'] = "true"
+    Setting.plugin_redmine_banner['start_ymd'] = start_time.strftime("%Y-%m-%d")
+    Setting.plugin_redmine_banner['start_hour'] = start_time.strftime("%H")
+    Setting.plugin_redmine_banner['start_min'] = start_time.strftime("%M")
+
+    Setting.plugin_redmine_banner['end_ymd'] = end_time.strftime("%Y-%m-%d")
+    Setting.plugin_redmine_banner['end_hour'] = end_time.strftime("%H")
+    Setting.plugin_redmine_banner['end_min'] = end_time.strftime("%M")    
+    assert(target.should_display_header?(context))
+  end
+    
+  def  test_should_display_footer?
+    target = BannerMessageHooks.instance
+    context = {}
+    Setting.plugin_redmine_banner['enable'] = "true"
+    Setting.plugin_redmine_banner['display_part'] = "header"
+    Setting.plugin_redmine_banner['use_timer'] = "false"   
+    assert(!target.should_display_footer?(context))    
+  end
 end

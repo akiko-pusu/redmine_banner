861e5fa2e46e7193fbe93ab0757cc8167571d787
Akiko Takano <akiko.pusu@gmail.com>
2012-02-21 13:49:37 +0900

    Add test code for routing check, response check.
    Signed-off-by: Akiko Takano <akiko.pusu@gmail.com>

--- test/functional/banner_controller_test.rb
+++ test/functional/banner_controller_test.rb
@@ -1,8 +1,30 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class BannerControllerTest < ActionController::TestCase
+  def setup
+    User.current = nil
+    @request.session[:user_id] = 1 # Do test as admin
+  end  
+  
   # Replace this with your real tests.
   def test_truth
     assert true
+  end
+  
+  test "should off banner" do
+    get :off
+    assert_response :success, "returned #{@response}"
+    assert_equal "false", Setting.plugin_redmine_banner['enable']
+  end
+  
+  test "routing check" do
+    assert_generates('banner/preview', { :controller => 'banner', :action => 'preview'})
+    assert_generates('banner/off', { :controller => 'banner', :action => 'off'})
+  end
+  
+  test "should preview banner" do
+    get :preview, {:settings => {:banner_description=> "h1. Test data."}}
+    assert_template "common/_preview.html.erb"
+    assert_select 'h1', /Test data\./, "#{@response.body}"
   end
 end

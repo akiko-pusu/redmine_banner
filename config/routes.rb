2bf96e3e498378ae0b4a1efaf81a2cc61dd35b9a
Akiko Takano <akiko.pusu@gmail.com>
2012-04-04 08:53:37 +0900

    Fixed Bug for routing.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ ActionController::Routing::Routes.draw do |map|
   map.connect 'banner/preview', :controller => 'banner', :action => 'preview'
   map.connect 'banner/off', :controller => 'banner', :action => 'off'
   map.connect 'projects/:project_id/banner/:action', :controller => 'banner'
-  map.connect 'projects/:project_id/banner/off', :controller => 'banner', :action => 'project_banner_off'
+  map.connect 'projects/:project_id/banner/project_banner_off', :controller => 'banner', :action => 'project_banner_off'
 end

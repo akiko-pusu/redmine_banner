331c6cb1371fb4cf3ab56934bd3df5876af305fb
Akiko Takano <akiko.pusu@gmail.com>
2012-04-04 14:10:01 +0900

    Revert "Modified for testing display_location and style."
    This reverts commit 6801ddf2db5b1abe4353cb983e2489ec5fd43a08.

--- test/functional/projects_controller_test.rb
+++ test/functional/projects_controller_test.rb
@@ -6,7 +6,7 @@ class ProjectsController; def rescue_action(e) raise e end; end
 
 class ProjectsControllerTest < ActionController::TestCase
   fixtures :projects, :users, :roles, :members, :member_roles,
-           :trackers, :projects_trackers, :enabled_modules, :banners
+           :trackers, :projects_trackers, :enabled_modules
   def setup
     @controller = ProjectsController.new
     @request    = ActionController::TestRequest.new
@@ -17,11 +17,7 @@ class ProjectsControllerTest < ActionController::TestCase
     # Enabled Banner module
     @project = Project.find(1)
     @project.enabled_modules << EnabledModule.new(:name => 'banner')
-    @project.save!
-
-    @banner = Banner.find_or_create(1)
-    @banner.display_part = "overview"
-    @banner.save!
+    @project.save!    
   end
   
   def test_settings
@@ -29,16 +25,6 @@ class ProjectsControllerTest < ActionController::TestCase
     assert_response :success
     assert_template 'settings'
     assert_select 'a#tab-banner'
-    assert_select 'div#project_banner_area div.banner_info', false, 'Banner should be displayed Overview only.'
-  end
-
-  # project 1 is enabled banner and type is info, display_part is overview only.
-  def test_show_overview
-    get :show, :id => 1
-    assert_response :success
-    assert_select 'div#project_banner_area div.banner_info'    
-  end
-
-  
+  end  
 end
 

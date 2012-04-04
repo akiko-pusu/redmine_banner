6498e77f9abe2775ab6d1c78e490183919e0184e
Akiko Takano <akiko.pusu@gmail.com>
2012-04-04 14:12:51 +0900

    Modified for testing display_location and style.

--- test/functional/banner_controller_test.rb
+++ test/functional/banner_controller_test.rb
@@ -1,62 +1,72 @@
 require File.dirname(__FILE__) + '/../test_helper'
-
 class BannerControllerTest < ActionController::TestCase
-#  fixtures :users
-  fixtures :projects, 
-    :users, 
-    :roles, 
-    :members, 
-    :member_roles, 
-    :enabled_modules, 
-    :banners
-  
+  fixtures :users
   def setup
+    User.current = nil
+    @request.session[:user_id] = 1 # Do test as admin
+    @settings = Setting["plugin_redmine_banner"]
+    @settings["enable"] = "true";
+  end  
+  
+  test "should off banner" do
+    get :off
+    assert_response :success, "returned #{@response}"
+    assert_equal "false", @settings["enable"]
   end
   
-  context "#admin" do
-    def setup
-      User.current = nil
-      @request.session[:user_id] = 1 # Do test as admin
-      @settings = Setting["plugin_redmine_banner"]
-      @settings["enable"] = "true";
-    end  
-
-    should "should off banner" do
-      get :off
-      assert_response :success, "returned #{@response}"
-      assert_equal "false", @settings["enable"]
-    end
-
-    should "routing check" do
-      assert_generates('banner/preview', { :controller => 'banner', :action => 'preview'})
-      assert_generates('banner/off', { :controller => 'banner', :action => 'off'})
-    end
-
-    should "should preview banner" do
-      get :preview, {:settings => {:banner_description=> "h1. Test data."}}
-      assert_template "common/_preview.html.erb"
-      assert_select 'h1', /Test data\./, "#{@response.body}"
-    end
+  test "routing check" do
+    assert_generates('banner/preview', { :controller => 'banner', :action => 'preview'})
+    assert_generates('banner/off', { :controller => 'banner', :action => 'off'})
   end
   
-  context "#project_admin" do
-    context "with permission" do
+  test "should preview banner" do
+    get :preview, {:settings => {:banner_description=> "h1. Test data."}}
+    assert_template "common/_preview.html.erb"
+    assert_select 'h1', /Test data\./, "#{@response.body}"
+  end
+  
+  ### test for project banner
+  context "#project_banner" do
+    setup do
+      @request.session[:user_id] = 2
+      @response   = ActionController::TestResponse.new
+      @request.env["HTTP_REFERER"] = '/'
+      # Enabled Banner module
+      @project = Project.find(1)
+      @project.enabled_modules << EnabledModule.new(:name => 'banner')
+      @project.save!
+    end
+    
+     should "edit without manage permission return 403" do
+      Role.find(1).remove_permission! :manage_banner
+      post :edit, :project_id => 1, 
+        :settings => { :enabled => "1", :description => "Edit test", :use_timer => false, 
+        :display_part => "all", :style => 'alert'}
+      assert_response 403
+    end
+
+    context "with permission" do	
       setup do
-        @request.session[:user_id] = 2
-        # Enabled Banner module
-        @project = Project.find(1)
-        @project.enabled_modules << EnabledModule.new(:name => 'banner')
-        @project.save!          
-        Role.find(1).add_permission! :manage_banner          
+        Role.find(1).add_permission! :manage_banner
+        @project = Project.find 1
       end
 
+      should "non existing project return 404" do
+        # set non existing project
+        post :edit, :project_id => 100, 
+          :settings => { :enabled => "1", :description => "Edit test", :use_timer => false, 
+          :display_part => "all", :style => 'alert'}
+        assert_response 404        
+      end
+      
       should "redirect post" do
-        post :edit, :project_id => @project, 
-          :settings => { :enabled => true, :display_part => 'overview', :description => 'display test'}, :tab => "banner"
+        post :edit, :project_id => 1, 
+          :settings => { :enabled => "1", :description => "Edit test", :use_timer => false, 
+          :display_part => "all", :style => 'alert'}
         assert_response :redirect          
         assert_redirected_to :controller => 'projects', 
           :action => "settings", :id => @project, :tab => 'banner'
-      end
-    end 
-  end  
+      end      
+    end
+  end
 end

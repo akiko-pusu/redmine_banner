3ddbcb270eeba151c2caf7749ec196f20245ef79
Akiko Takano <akiko.pusu@gmail.com>
2012-07-05 05:49:29 +0900

    Rename rhtml to html.erb. Now preview is available. (IssueID: #1077)
    Signed-off-by: Akiko Takano <akiko.pusu@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do 
   match 'projects/:project_id/banner/:action', :to => 'banner'
   match 'projects/:project_id/banner/project_banner_off', :to => 'banner#project_banner_off'
-  match 'banner/preview', :to => 'banner#preview'
+  match 'banner/preview', :to => 'banner#preview',:via => [:get, :post]
   match 'banner/off', :to => 'banner#off'
+  match 'projects/:project_id/banner/preview', :to => 'banner#preview', :via => [:get, :post]
 end

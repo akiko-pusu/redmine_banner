41cf687606c69f9f942be357218809fc42cb8e4c
Akiko Takano <akiko.pusu@gmail.com>
2015-01-01 16:53:47 +0900

    Ready for Redmine3.0, Rails4.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do 
-  match 'projects/:project_id/banner/:action', :to => 'banner'
-  match 'projects/:project_id/banner/project_banner_off', :to => 'banner#project_banner_off'
+  match 'projects/:project_id/banner/:action', :to => 'banner', :via => [:get, :post, :patch]
+  match 'projects/:project_id/banner/project_banner_off', :to => 'banner#project_banner_off', :via => [:get]
   match 'banner/preview', :to => 'banner#preview',:via => [:get, :post]
-  match 'banner/off', :to => 'banner#off'
+  match 'banner/off', :to => 'banner#off', :via => [:get, :post]
   match 'projects/:project_id/banner/preview', :to => 'banner#preview', :via => [:get, :post]
 end

2e6794bb2366b0d7b14840b97b27aaa01cd2bbe0
Akiko Takano <akiko.pusu@gmail.com>
2015-11-23 00:07:41 +0900

    Update test code. (Now, 2015!)

--- test/functional/banner_setting_controller_test.rb
+++ test/functional/banner_setting_controller_test.rb
@@ -51,10 +51,13 @@ class BannerSettingControllerTest < ActionController::TestCase
   
   def test_post_banner_settings_with_good_format
     # set good format
+    now = Date.today
+    afterDay = now + 1
+
     post :plugin, :id => "redmine_banner",
-      :settings => {:end_ymd => "2015-03-31", :end_min => "03", :start_min => "03", :start_hour => "20", 
+      :settings => {:end_ymd => afterDay.strftime("%Y-%m-%d"), :end_min => "03", :start_min => "00", :start_hour => "00",
       :enable => "true", :type => "warn", :display_part => "both", 
-      :start_ymd => "2012-03-12", :use_timer => "true",
+      :start_ymd => now.strftime("%Y-%m-%d"), :use_timer => "true",
       :banner_description => "exp. Information about upcoming Service Interruption.", 
       :end_hour => "23"}
     assert_response :redirect

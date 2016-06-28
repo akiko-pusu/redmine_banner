d4b6e6b15e208d21284e543d90c9e2faea6a43d6
admin <akiko.pusu@gmail.com>
2016-06-29 07:49:36 +0900

    Refactoring (indent style and applied rubocop detection)

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -4,33 +4,30 @@ class Banner < ActiveRecord::Base
   belongs_to :project
 
   validates_uniqueness_of :project_id
-  validates :project_id, :presence => true
-  validates_inclusion_of :display_part, :in=> ['all', 'new_issue', 'overview','overview_and_issues']
-  validates_inclusion_of :style, :in=> ['info','warn','alert', 'normal', 'nodata']
+  validates :project_id, presence: true
+  validates_inclusion_of :display_part, in: %w(all new_issue overview overview_and_issues)
+  validates_inclusion_of :style, in: %w(info warn alert normal nodata)
   # project should be stable.
   safe_attributes 'banner_description', 'style', 'start_date', 'end_date', 'enabled', 'use_timer', 'display_part'
   attr_accessible :enabled, :style, :display_part, :banner_description
-  
-  def self.find_or_create(project_id)	
-    banner = Banner.where(['project_id = ?', project_id]).first()
+
+  def self.find_or_create(project_id)
+    banner = Banner.where(['project_id = ?', project_id]).first
     unless banner.present?
       banner = Banner.new
       banner.project_id = project_id
       banner.enabled = false
-      
+
       # Set default (Also set default by migration file)
-      banner.display_part = "all"
-      banner.style = "info"
-      banner.save!      
+      banner.display_part = 'all'
+      banner.style = 'info'
+      banner.save!
     end
-    return banner
-  end
-  
-  def enable_banner?
-    if self.enabled == true && !self.banner_description.blank?
-      return true
-    end
-    return false
+    banner
   end
 
+  def enable_banner?
+    return true if enabled == true && !banner_description.blank?
+    false
+  end
 end

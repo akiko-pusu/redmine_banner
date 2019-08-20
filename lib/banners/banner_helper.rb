ba64070a2f753191eaa6460ec83a793ebefed3a0
Akiko Takano <akiko.pusu@gmail.com>
2019-08-20 10:44:56 +0900

    Fixed format. Update rubocop setting.

--- lib/banners/banner_helper.rb
+++ lib/banners/banner_helper.rb
@@ -1,12 +1,15 @@
+# frozen_string_literal: true
+
 module Banners
   module BannerHelper
-    def get_time(ymd, h, m)
+    def get_time(ymd, hour, minute)
       begin
         d = Date.strptime(ymd, '%Y-%m-%d')
-      rescue
+      rescue StandardError => e
+        logger.warn("Passed value #{ymd} for Banner has wrong format. #{e.message}")
         d = Date.current
       end
-      Time.mktime(d.year, d.month, d.day, h.to_i, m.to_i)
+      Time.mktime(d.year, d.month, d.day, hour.to_i, minute.to_i)
     end
 
     def enabled?(project)

505ad49670d31f1e35a637550a39e9c3cf25e31c
Akiko Takano <akiko.pusu@gmail.com>
2019-08-27 12:34:57 +0900

    Add sample user.

--- script/heroku_runner.rb
+++ script/heroku_runner.rb
@@ -3,20 +3,37 @@
 api_key = ENV['BANNER_API_KEY']
 pass = ENV['BANNER_VER']
 
-puts api_key
-
 admin = User.find_by_login('admin')
 admin.update!(password: pass, must_change_passwd: false)
 version = ENV['BANNER_VER']
 
 Setting.rest_api_enabled = '1'
 
-if admin.api_token.nil?
-  admin.send(:api_key)
+# set banner test user
+banner_user = User.find_by_login('banner_test')
+if banner_user.blank?
+  banner_user = User.new(firstname: 'banner', lastname: 'test', login: 'banner_test', status: 1)
+  banner_user.mail = 'banner_test@example.com'
+  banner_user.save
 end
 
-token = Token.where(action: 'api').where(user_id: admin.id).first
+# find or create for GlobalBanner Admin group
+banner_group = Group.find_by_lastname('GlobalBanner_Admin')
+if banner_group.blank?
+  banner_group = Group.create(name: 'GlobalBanner_Admin')
+end
+banner_group.user_ids = [banner_user.id]
+banner_group.save
+
+# Update user's api
+if banner_user.api_token.nil?
+  banner_user.send(:api_key)
+end
+
+token = Token.where(action: 'api').where(user_id: banner_user.id).first
 token.update_attributes(value: api_key) unless token.blank?
+banner_user.api_token = token
+banner_user.save
 
 description = 'This is a test message for Global Banner. '
 description += "(#{version} / Redmine trunk: #{Redmine::VERSION::REVISION || Redmine::VERSION})"
